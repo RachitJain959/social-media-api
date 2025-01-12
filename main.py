0fb55e32a01e0646184715216cd5ef5877933298
Rachit Jain <rachitjain9596@gmail.com>
2025-01-13 03:05:06 +0530

    create http reqs

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello world"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "These are all your posts."}
+
+@app.post("/createposts")
+def create_posts(payLoad: dict = Body(...)):
+    print(payLoad)
+    return {"message": "successfully created the post!"}
--- readme.md
+++ readme.md
@@ -13,3 +13,14 @@
     -   select python interpretor for this venv from view->command palette-> select py interp
 
 -   pip install fastapi[all]
+-   create http requests
+
+### Code Snippets
+
+```python
+@app.get("/") #decorator
+```
+
+-   app is the instance of fastapi
+-   get is the http request
+-   "/" root path
