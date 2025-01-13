e6b7c571c4508f4c3eb998ab81d2f91417b4fbd3
Rachit Jain <rachitjain9596@gmail.com>
2025-01-14 01:32:57 +0530

    get post

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ class Post(BaseModel):
 
 my_posts = [{"title": "post1 title", "content": "post1 content", "id": 1}, {"title": "post 2 title", "content": "post 2 content", "id": 2}]
 
+def find_post(id):
+    for post in my_posts:
+        if post["id"] == id:
+            return post
+
 @app.get("/")
 async def root():
     return {"message": "Hello world"}
@@ -21,6 +26,10 @@ async def root():
 @app.get("/posts")
 def get_posts():
     return {"data": my_posts}
+
+@app.get("/posts/{id}")
+def get_post(id):
+    return {"post detail": find_post(int(id))}
 
 @app.post("/posts")
 def create_posts(post: Post):
