d40398f007ce2f5b4b753e84a503d784b6c02bd1
Rachit Jain <rachitjain9596@gmail.com>
2025-01-14 15:05:49 +0530

    storing posts

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
+
+app = FastAPI()
 
 class Post(BaseModel):
     title: str
@@ -9,7 +12,7 @@ class Post(BaseModel):
     published: bool =  True
     rating : Optional[int]= None
 
-app = FastAPI()
+my_posts = [{"title": "post1 title", "content": "post1 content", "id": 1}, {"title": "post 2 title", "content": "post 2 content", "id": 2}]
 
 @app.get("/")
 async def root():
@@ -17,9 +20,11 @@ async def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "These are all your posts."}
+    return {"data": my_posts}
 
-@app.post("/createposts")
-def create_posts(new_post: Post):
-    print(new_post)
-    return {"data": "new post"}
+@app.post("/posts")
+def create_posts(post: Post):
+    post_dict = post.model_dump()
+    post_dict['id'] = randrange(0, 10000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
