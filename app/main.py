9f1f8752a4ba07adb9f3f3ccaba0a3500b16219b
Rachit Jain <rachitjain9596@gmail.com>
2025-01-20 01:15:00 +0530

    get post

--- app/main.py
+++ app/main.py
@@ -58,8 +58,11 @@ def get_posts():
 
 # single post
 @app.get("/posts/{id}")
-def get_post(id: int, response: Response):
-    post = find_post(id)
+def get_post(id: int):
+    cursor.execute(""" SELECT * FROM posts WHERE id=%s """, (str(id)))
+    post = cursor.fetchone()
+
+
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail= f"post with id:{id} not found.")
