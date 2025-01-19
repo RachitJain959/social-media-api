3ad0558942481b30a46728ae8e55e3806a9956e1
Rachit Jain <rachitjain9596@gmail.com>
2025-01-20 01:42:09 +0530

    delete post

--- app/main.py
+++ app/main.py
@@ -62,7 +62,6 @@ def get_post(id: int):
     cursor.execute(""" SELECT * FROM posts WHERE id=%s """, (str(id)))
     post = cursor.fetchone()
 
-
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail= f"post with id:{id} not found.")
@@ -82,15 +81,17 @@ def create_post(post: Post):
 
     return {"data": new_post}
 
-# delte post
+# delete post
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
-    index = find_index(id)
-    if index == None: 
+    cursor.execute(""" DELETE FROM posts WHERE id = %s RETURNING *""", (str(id)))
+    deleted_post = cursor.fetchone()
+    connection.commit()
+
+    if deleted_post == None: 
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail= f"post with id:{id} not found.")
-    # print(index)
-    my_posts.pop(index)
+
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/posts/{id}")
