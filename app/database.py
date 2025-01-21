ee293a4877fed57ccc9bd9e2cc14216a47df0e7b
Rachit Jain <rachitjain9596@gmail.com>
2025-01-21 15:01:15 +0530

    database.py blueprint complete

--- app/database.py
+++ app/database.py
@@ -1,7 +1,20 @@
+from dotenv import load_dotenv
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://<username>:<password>@<ip-address/hostname>/<database_name>'
+load_dotenv()
+USERNAME = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
+IP_ADDRESS = os.getenv("IP_ADDRESS")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+
+SQLALCHEMY_DATABASE_URL = 'postgresql://USERNAME:PASSWORD@IP_ADDRESS/DATABASE_NAME'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
