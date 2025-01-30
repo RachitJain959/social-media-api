84621d1d9c68b97ab5e7fe5494265ea34c074c7d
Rachit Jain <rachitjain9596@gmail.com>
2025-01-30 16:20:31 +0530

    password verify

--- app/utils.py
+++ app/utils.py
@@ -3,3 +3,6 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash(password: str):
     return pwd_context.hash(password)
+
+def verify(input_password, hashed_password):
+    return pwd_context.verify(input_password, hashed_password)
