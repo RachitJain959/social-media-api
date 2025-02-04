46b8d65f65d84c83884acf80fe64b64c9db7f7d9
Rachit Jain <rachitjain9596@gmail.com>
2025-02-05 00:09:47 +0530

    retieve current users post

--- app/routers/post.py
+++ app/routers/post.py
@@ -12,21 +12,27 @@ def get_posts(db: Session = Depends(get_db),
                 current_user: int = Depends(oauth2.get_current_user)):
     
     posts = db.query(models.Post).all()
+    # display posts owned by current user
+    # posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id)
+
     return posts
 
 # single post
 @router.get("/{id}", response_model=schemas.Post)
 def get_post(id: int, db: Session = Depends(get_db),
                 current_user: int = Depends(oauth2.get_current_user)):
-    # cursor.execute(""" SELECT * FROM posts WHERE id=%s """, (str(id)))
-    # post = cursor.fetchone()
-
+ 
     post = db.query(models.Post).filter(models.Post.id == id).first()
 
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail= f"post with id:{id} not found.")
 
+    # Get particular post of current user. Cannot get posts from other user if enabled.
+    # if post.owner_id != current_user.id:
+    #     raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+    #                         detail="Not authorized to perform requested action.")
+    
     return post
 
 # create post
