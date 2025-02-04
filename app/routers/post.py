2e81b2d4e490816a230f4ad5f87f247eea09b1a5
Rachit Jain <rachitjain9596@gmail.com>
2025-02-04 15:38:19 +0530

    Fetching User in Protected Routes

--- app/routers/post.py
+++ app/routers/post.py
@@ -9,9 +9,10 @@ router = APIRouter(prefix="/posts", tags=['Posts'])
 # all posts
 @router.get("/", response_model=List[schemas.Post])
 def get_posts(db: Session = Depends(get_db),
-                user_id: int = Depends(oauth2.get_current_user)):
+                current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute(""" SELECT * FROM posts """)
     # posts = cursor.fetchall()
+    print(current_user.email)
     
     posts = db.query(models.Post).all()
     return posts
@@ -19,7 +20,7 @@ def get_posts(db: Session = Depends(get_db),
 # single post
 @router.get("/{id}", response_model=schemas.Post)
 def get_post(id: int, db: Session = Depends(get_db),
-                user_id: int = Depends(oauth2.get_current_user)):
+                current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute(""" SELECT * FROM posts WHERE id=%s """, (str(id)))
     # post = cursor.fetchone()
 
@@ -29,16 +30,14 @@ def get_post(id: int, db: Session = Depends(get_db),
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail= f"post with id:{id} not found.")
 
-        # response.status_code = status.HTTP_404_NOT_FOUND
-        # return {"error": f"post with id:{id} not found."}
     return post
 
 # create post
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
 def create_post(post: schemas.PostCreate, db: Session = Depends(get_db),
-                user_id: int = Depends(oauth2.get_current_user)):
+                current_user: int = Depends(oauth2.get_current_user)):
     
-    print(user_id)
+    print(current_user.email)
     new_post = models.Post(**post.model_dump())
     db.add(new_post)
     db.commit()
@@ -49,7 +48,7 @@ def create_post(post: schemas.PostCreate, db: Session = Depends(get_db),
 # delete post
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db),
-                user_id: int = Depends(oauth2.get_current_user)):
+                current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute(""" DELETE FROM posts WHERE id = %s RETURNING *""", (str(id)))
     # deleted_post = cursor.fetchone()
     # connection.commit()
@@ -66,7 +65,8 @@ def delete_post(id: int, db: Session = Depends(get_db),
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @router.put("/{id}", response_model=schemas.Post)
-def update_post(id:int, updated_post:schemas.PostCreate, db: Session = Depends(get_db)):
+def update_post(id:int, updated_post:schemas.PostCreate, db: Session = Depends(get_db), 
+                current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute(""" UPDATE posts SET title=%s, content=%s, published=%s WHERE id=%s RETURNING * """, 
     #                (post.title, post.content, post.published, str(id)))
     
@@ -76,7 +76,6 @@ def update_post(id:int, updated_post:schemas.PostCreate, db: Session = Depends(g
     post_query = db.query(models.Post).filter(models.Post.id == id)
     post = post_query.first()
    
-    # print(type(post)) 
     if post == None: 
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail= f"post with id:{id} not found.")
@@ -85,6 +84,7 @@ def update_post(id:int, updated_post:schemas.PostCreate, db: Session = Depends(g
     db.commit()
 
     return post_query.first()
+
 # def update_post(id: int, post: Post, db: Session = Depends(get_db)):
     # Get the existing SQLAlchemy post
     db_query = db.query(models.Post).filter(models.Post.id == id)
